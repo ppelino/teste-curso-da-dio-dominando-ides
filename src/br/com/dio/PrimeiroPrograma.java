de3e5c3bfb81e8cfc095f2533dd63a714f9152c1
Perkles <ppelino@hotmail.com>
2022-02-17 14:48:03 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new gato();
+        Systen.out.println(gato);
+        System.out.println("hello Word!" + (a + b));
+
+        int a = 5;
+        int b = 3;
+
+
+        {
+        }
+    }
